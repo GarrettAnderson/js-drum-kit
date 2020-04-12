ca4791a497b94e3293a65183305030a93b00f8b7
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-12 14:07:17 -0400

    added class name of playing when speific key is pressed

--- public/main.js
+++ public/main.js
@@ -1,7 +1,10 @@
 window.addEventListener('keydown', function(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
   console.log(audio)
   if (!audio) return //stop the function from running all together
   audio.currentTime = 0 // rewind to start
   audio.play()
+  console.log(key)
+  key.classList.add('playing')
 })
