c4274e3e0bb2f01b0ac9b0503384b94f0f83ad7e
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-12 13:57:20 -0400

    got the sounds to play when corresponding key is played

--- public/main.js
+++ public/main.js
@@ -2,4 +2,5 @@ window.addEventListener('keydown', function(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
   console.log(audio)
   if (!audio) return //stop the function from running all together
+  audio.play()
 })
