c44caf076c74bc7b35d87d3fe0b3da687738c989
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-12 14:02:22 -0400

    set audio.currentTime to 0

--- public/main.js
+++ public/main.js
@@ -2,5 +2,6 @@ window.addEventListener('keydown', function(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
   console.log(audio)
   if (!audio) return //stop the function from running all together
+  audio.currentTime = 0 // rewind to start
   audio.play()
 })
