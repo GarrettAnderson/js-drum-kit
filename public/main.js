828b200891b99180524aac7b25873406774a613f
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-11-12 19:55:49 -0500

    got netlify working so I can host the project online. woring on creating the JS for the project. I was able to attach an event listener to test the keyboard key click triggering a function. now working on getting the console to print a test phrase when a keyboard key is pressed.

--- public/main.js
+++ public/main.js
@@ -1,21 +1,81 @@
-function playSound(event) {
-  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
-  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
-  console.log(audio)
-  if (!audio) return //stop the function from running all together
-  audio.currentTime = 0 // rewind to start
-  audio.play()
-  console.log(key)
-  key.classList.add('playing')
-}
-function removeTransition(event) {
+
+
+// user is able to click on letter keyboard button to play a sound
+
+
+function keyPressed(event) {
+  console.log('testing')
   console.log(event)
-  if (event.propertyName !== 'transform') return // skip it if it's not a transform
-  console.log(this)
-  this.classList.remove('playing')
+
+
 }
 
-const keys = document.querySelectorAll('.key')
-keys.forEach((key) => key.addEventListener('transitionend', removeTransition))
-console.log(keys)
-window.addEventListener('keydown', playSound)
+let allKeys = document.querySelectorAll('.key')
+console.log(allKeys)
+// currentKey.addEventListener('click', keyPressed)
+
+let dataKey = document.querySelectorAll('audio')
+
+allKeys.forEach(
+  function (keyInAllKeys) {
+    document.addEventListener('keydown', keyPressed)
+    console.log(keyInAllKeys)
+
+  }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ============================
+// ============================
+
+
+// function playSound(event) {
+//   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+//   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
+//   console.log(audio)
+//   if (!audio) return //stop the function from running all together
+//   audio.currentTime = 0 // rewind to start
+//   audio.play()
+//   console.log(key)
+//   key.classList.add('playing')
+// }
+// function removeTransition(event) {
+//   console.log(event)
+//   if (event.propertyName !== 'transform') return // skip it if it's not a transform
+//   console.log(this)
+//   this.classList.remove('playing')
+// }
+//
+// const keys = document.querySelectorAll('.key')
+// keys.forEach((key) => key.addEventListener('transitionend', removeTransition))
+// console.log(keys)
+// window.addEventListener('keydown', playSound)
