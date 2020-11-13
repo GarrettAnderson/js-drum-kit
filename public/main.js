586f31c197de31dd0e7f7706ce424661941c7428
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-11-12 20:46:44 -0500

    wrote more JS in order to isolate the data-key values for the letter elements along with the data-key values for the audio elements. Updated the keydown function to play audio but need to work on getting the correct sound to play per key pressed. created global variables to pass data to the keyPressed function

--- public/main.js
+++ public/main.js
@@ -2,25 +2,56 @@
 
 // user is able to click on letter keyboard button to play a sound
 
+let dataAttributeValue;
+let audioDataAttributeValue;
+let audioFile;
 
 function keyPressed(event) {
-  console.log('testing')
+  // console.log('testing')
   console.log(event)
+  console.log(dataAttributeValue)
+  console.log(audioDataAttributeValue)
+  console.log(audioFile)
 
+  // If the data-key value of a letter elements is equal to the data-key value of the audio elements, play that specific audio file
 
+  if (dataAttributeValue = audioDataAttributeValue) {
+    console.log('play the audio file')
+    audioFile.play()
+  } else (
+    console.log('no audio file to play')
+  )
 }
 
 let allKeys = document.querySelectorAll('.key')
 console.log(allKeys)
+
 // currentKey.addEventListener('click', keyPressed)
 
-let dataKey = document.querySelectorAll('audio')
+let audioDataKeys = document.querySelectorAll('audio[data-key]')
+console.log(audioDataKeys)
 
-allKeys.forEach(
+allKeys.forEach (
   function (keyInAllKeys) {
-    document.addEventListener('keydown', keyPressed)
-    console.log(keyInAllKeys)
 
+    document.addEventListener('keydown', keyPressed)
+
+      console.log(keyInAllKeys)
+
+    var dataKeyValue = keyInAllKeys.getAttribute('data-key')
+
+    dataAttributeValue = dataKeyValue
+  }
+)
+
+audioDataKeys.forEach (
+  function (audio) {
+    console.log(audio)
+
+    var audioDataKeyValue = audio.getAttribute('data-key')
+
+    audioDataAttributeValue = audioDataKeyValue
+    audioFile = audio
   }
 )
 
